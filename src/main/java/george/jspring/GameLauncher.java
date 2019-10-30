886eb4e721e6b0b28798a864fad18dcae3e4b924
George <31487809+GeoFoxit@users.noreply.github.com>
2019-10-30 21:16:28 +0200

    Implement DI via setter
    - add setter to GameLauncher.java
    - remove constructor from GameLauncher.java
    - modify xml config

--- src/main/java/george/jspring/GameLauncher.java
+++ src/main/java/george/jspring/GameLauncher.java
@@ -2,9 +2,14 @@ package george.jspring;
 
 public class GameLauncher {
     private Game game;
+    private String user;
 
-    public GameLauncher(Game game) {
+    public void setGame(Game game) {
         this.game = game;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
     }
 
     public void play() {
